58b17415bb20e3dc9cda6a38157a1698b0ac000f
Lee ChaeHyeon <chlee1001@naver.com>
2020-11-22 00:29:49 +0900

    Add knn,gbk Classification Algorithm 20.11.22

--- Classification/Classification.py
+++ Classification/Classification.py
@@ -9,21 +9,18 @@ from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier, VotingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import KFold
+from sklearn.model_selection import GridSearchCV, cross_val_score, KFold
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC
 
 df = pd.read_csv('bank.csv')
 
-
 print("\n--------------------------- < Initial data > ----------------------------")
 print(df)
 print("-------------------------------------------------------------------------\n")
 # Dirty data count output
 print("In Initial data, total dirty data count = ", sum(df.isna().sum()))
-
 
 # ──────────────────────────────────────────
 #          Drop Columns
@@ -41,8 +38,8 @@ for c in df.columns:
 
 df2 = df.copy()
 
-#Drop unnecessary columns
-df2.drop(['Unnamed: 0', 'marital',  'marital', 'education', 'contact', 'month', 'day_of_week',
+# Drop unnecessary columns
+df2.drop(['Unnamed: 0', 'marital', 'marital', 'education', 'contact', 'month', 'day_of_week',
           'duration', 'campaign', 'pdays', 'previous', 'poutcome'], axis=1, inplace=True)
 
 print("\n--------------------------- < After Drop Columns > ----------------------------")
@@ -61,7 +58,6 @@ for c in df2.columns:
 # ──────────────────────────────────────────
 df2 = df2.apply(LabelEncoder().fit_transform)
 
-
 # ──────────────────────────────────────────
 #      Show Correlation with Heatmap
 # ──────────────────────────────────────────
@@ -69,7 +65,7 @@ corrmat = df2.corr()
 top_corr = corrmat.index
 plt.figure(figsize=(9, 9))
 g = sns.heatmap(df2[top_corr].corr(), annot=True, cmap="RdYlGn")
-#plt.show()
+# plt.show()
 
 
 # ──────────────────────────────────────────
@@ -79,7 +75,8 @@ y = df2['y']
 X = df2.drop(['y'], axis=1)
 scaler = MinMaxScaler()
 scaled_x = scaler.fit_transform(X)
-scaled_x = pd.DataFrame(scaled_x, columns=['age', 'job', 'default', 'housing', 'loan', 'emp.var.rate', 'cons.price.idx', 'cons.conf.idx', 'euribor3m', 'nr.employed'])
+scaled_x = pd.DataFrame(scaled_x, columns=['age', 'job', 'default', 'housing', 'loan', 'emp.var.rate', 'cons.price.idx',
+                                           'cons.conf.idx', 'euribor3m', 'nr.employed'])
 target_names = ['No', 'Yes']
 
 # Visualize Data Normalization with MinMax Scaling
@@ -110,15 +107,14 @@ sns.kdeplot(scaled_x['nr.employed'], ax=ax2)
 
 plt.show()
 
-
 # ──────────────────────────────────────────
 #               Split data
 # ──────────────────────────────────────────
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(scaled_x, y, test_size=0.1, shuffle=True, random_state=0)
+
+X_train, X_test, y_train, y_test = train_test_split(scaled_x, y, test_size=0.2, shuffle=True, random_state=0)
 
 kf = KFold(n_splits=5)
-
 
 # ──────────────────────────────────────────
 #               Decision Tree
@@ -129,7 +125,6 @@ dt_clf = DecisionTreeClassifier(max_depth=5, criterion='entropy', random_state=0
 dt_clf.fit(X_train, y_train)
 
 y_pred = dt_clf.predict(X_test)
-
 
 dt_acc = float(accuracy_score(y_test, y_pred).round(3))
 dt_mse = float(metrics.mean_squared_error(y_test, y_pred).round(3))
@@ -162,7 +157,6 @@ print('\n----------------- < Logistic Regression > -----------------')
 print("Best parameters:", reg_clf.best_params_)
 print("Best score:", reg_clf.best_score_.round(3))
 
-
 best_reg_clf = LogisticRegression(**reg_clf.best_params_)
 best_reg_clf.fit(X_train, y_train)
 y_pred = best_reg_clf.predict(X_test)
@@ -183,7 +177,6 @@ plt.show()
 print("\nLogistic Regression Classification Report")
 print(classification_report(y_test, y_pred, target_names=target_names, digits=3, zero_division=1))
 
-
 # ──────────────────────────────────────────
 #              Random forest
 # ──────────────────────────────────────────
@@ -198,11 +191,9 @@ print('\n----------------- < Random forest > -----------------')
 print("Best parameters:", ran_clf.best_params_)
 print("Best score:", ran_clf.best_score_)
 
-
 best_ran_clf = RandomForestClassifier(**ran_clf.best_params_)
 best_ran_clf.fit(X_train, y_train)
 y_pred = best_ran_clf.predict(X_test)
-
 
 ran_acc = float(accuracy_score(y_test, y_pred).round(3))
 ran_mse = float(metrics.mean_squared_error(y_test, y_pred).round(3))
@@ -218,6 +209,90 @@ plt.ylabel('Actual')
 plt.xlabel('Predicted')
 plt.show()
 print("\nRandom Forest Classification Report")
+print(classification_report(y_test, y_pred, target_names=target_names, digits=3, zero_division=1))
+
+# ──────────────────────────────────────────
+#                   KNN
+# ──────────────────────────────────────────
+# Neighbors
+neighbors = np.arange(0, 30)
+# Create empty list that will hold cv scores
+cv_scores = []
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knnClassfier = KNeighborsClassifier()
+parameters = {'weights': ['uniform', 'distance'],
+              'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']}
+
+knn_clf = GridSearchCV(knnClassfier, parameters, cv=kf)
+knn_clf.fit(X_train, y_train)
+print('\n----------------- < KNN > -----------------')
+print("Best parameters:", knn_clf.best_params_)
+print("Best score:", knn_clf.best_score_)
+
+# Perform 10-fold cross validation on training set for odd values of k:
+for k in neighbors:
+    k_value = k + 1
+    knn = KNeighborsClassifier(n_neighbors=k_value, algorithm='brute', weights='uniform', p=2, metric='euclidean')
+    kfold = KFold(n_splits=10, random_state=123)
+    scores = cross_val_score(knn, X_train, y_train, cv=kfold, scoring='accuracy')
+    cv_scores.append(scores.mean() * 100)
+    # print("k=%d %0.2f (+/- %0.2f)" % (k_value, scores.mean()*100, scores.std()*100))
+
+optimal_k = neighbors[cv_scores.index(max(cv_scores))]
+
+print("The optimal number of neighbors is %d with %0.1f%%" % (optimal_k, cv_scores[optimal_k]))
+# plt.plot(neighbors, cv_scores)
+# plt.xlabel('Number of Neighbors K')
+# plt.ylabel('Train Accuracy')
+# plt.show()
+
+
+knnClassfier = KNeighborsClassifier(n_neighbors=optimal_k)
+knnClassfier.fit(X_train, y_train)
+y_pred = knnClassfier.predict(X_test)
+knn_acc = float(accuracy_score(y_test, y_pred).round(3))
+knn_mse = float(metrics.mean_squared_error(y_test, y_pred).round(3))
+knn_f1 = float(metrics.f1_score(y_test, y_pred, average='weighted', zero_division=1).round(3))
+print("Accuracy:", knn_acc)
+print("MSE(Mean Square Error): ", knn_mse)
+print("F1 Score: ", knn_f1)
+
+cf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(cf_matrix, annot=True, cmap="YlGnBu")
+plt.title("KNN Confusion Matrix")
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.show()
+print("\nKNN Classification Report")
+print(classification_report(y_test, y_pred, target_names=target_names, digits=3, zero_division=1))
+
+# ──────────────────────────────────────────
+#                   GradientBoosting
+# ──────────────────────────────────────────
+from sklearn.ensemble import GradientBoostingClassifier
+
+gbkClassifier = GradientBoostingClassifier()
+gbkClassifier.fit(X_train, y_train)
+y_pred = gbkClassifier.predict(X_test)
+
+print('\n----------------- < GradientBoosting > -----------------')
+
+gbk_acc = float(accuracy_score(y_test, y_pred).round(3))
+gbk_mse = float(metrics.mean_squared_error(y_test, y_pred).round(3))
+gbk_f1 = float(metrics.f1_score(y_test, y_pred, average='weighted', zero_division=1).round(3))
+print("Accuracy:", gbk_acc)
+print("MSE(Mean Square Error): ", gbk_mse)
+print("F1 Score: ", gbk_f1)
+
+cf_matrix = confusion_matrix(y_test, y_pred)
+sns.heatmap(cf_matrix, annot=True, cmap="YlGnBu")
+plt.title("GradientBoosting Confusion Matrix")
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.show()
+print("\nGradientBoosting Classification Report")
 print(classification_report(y_test, y_pred, target_names=target_names, digits=3, zero_division=1))
 
 # ──────────────────────────────────────────
@@ -254,15 +329,16 @@ plt.show()
 print("\nSVM Classification Report")
 print(classification_report(y_test, y_pred, target_names=target_names, digits=3, zero_division=1))
 
-
 # ──────────────────────────────────────────
 #                   Result
 # ──────────────────────────────────────────
 print('\n----------------- < Result > -----------------')
-data = [['Decision Tree', dt_acc, dt_mse, dt_f1],
-        ['Logistic Regression', reg_acc, reg_mse, reg_f1],
-        ['Random Forest', ran_acc, ran_mse, ran_f1],
-        ['SVM', svm_acc, svm_mse, svm_f1]]
+data = [
+    ['Decision Tree', dt_acc, dt_mse, dt_f1],
+    ['Logistic Regression', reg_acc, reg_mse, reg_f1],
+    ['Random Forest', ran_acc, ran_mse, ran_f1],
+    ['SVM', svm_acc, svm_mse, svm_f1],
+    ['KNN', knn_acc, knn_mse, knn_f1],
+    ['GradientBoosting', gbk_acc, gbk_mse, gbk_f1]]
 result = pd.DataFrame(data, columns=['Algorithm', 'Accuracy', 'MSE', 'F1-score'])
 print(result)
-
